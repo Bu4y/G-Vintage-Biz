9d6233ef034bb43e055065aea020419d877f1294
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-10-05 11:41:09 +0700

    update onesignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { LoginPage } from '../pages/login/login';
 import { Component } from '@angular/core';
+import { OneSignal } from '@ionic-native/onesignal';
 import { Platform, MenuController, LoadingController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,19 +20,37 @@ export class MyApp {
     statusBar: StatusBar,
     splashScreen: SplashScreen,
     public menuController: MenuController,
-    public loadingCtrl: LoadingController
+    public loadingCtrl: LoadingController,
+    private oneSignal: OneSignal
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.initOnesignal();
     });
     this.user = JSON.parse(window.localStorage.getItem('jjuserbuyer'));
     if (this.user) {
       this.rootPage = TabsPage;
     }
     this.workaroundSideMenu();
+  }
+
+  initOnesignal() {
+    this.oneSignal.startInit('fdfae3dc-e634-47f4-b959-f04e60f4613b', '464766391164');
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+    this.oneSignal.handleNotificationReceived().subscribe(() => {
+      // do something when notification is received
+    });
+
+    this.oneSignal.handleNotificationOpened().subscribe(() => {
+      // do something when a notification is opened
+    });
+
+    this.oneSignal.endInit();
   }
 
   initLoadStoreList() {
@@ -48,7 +67,7 @@ export class MyApp {
         window.localStorage.removeItem('shop');
         this.rootPage = LoginPage;
         loading.dismiss();
-        
+
       });
     }
   }
@@ -71,7 +90,7 @@ export class MyApp {
     }, 1000);
   }
 
-  isShow(id){
+  isShow(id) {
     let shopId = window.localStorage.getItem('shop') ? JSON.parse(window.localStorage.getItem('shop'))._id : null;
     return shopId === id;
   }
