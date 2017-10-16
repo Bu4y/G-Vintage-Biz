287205f7f7076c1a0c76664030ba519e2a9888b6
Sirintra Wannakheaw <sirintrawannakeaw@gmail.com>
2017-10-16 18:40:33 +0700

    update home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { LoadingController, NavController, Platform, MenuController, App } from 'ionic-angular';
-import { OrderModel, OrderService, ShopModel } from "@ngcommerce/core";
+import { HomeService, ShopModel, HomeSellerModel } from "@ngcommerce/core";
 import { Chart } from 'chart.js';
 
 @Component({
@@ -8,16 +8,26 @@ import { Chart } from 'chart.js';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  homeData = {} as OrderModel;
-  waiting: number = 0;
-  accept: number = 0;
-  sent: number = 0;
-  return: number = 0;
-  total: number = 0;
-  percentWaiting: number = 0;
-  percentAccept: number = 0;
-  percentSent: number = 0;
-  percentReturn: number = 0;
+  homeData: any = {
+    items: {
+      day: { amount: 0 },
+      month: { amount: 0 },
+      year: { amount: 0 },
+      categories: [{
+        cate: ''
+      }]
+    },
+    report: []
+  };
+  // waiting: number = 0;
+  // accept: number = 0;
+  // sent: number = 0;
+  // return: number = 0;
+  // total: number = 0;
+  // percentWaiting: number = 0;
+  // percentAccept: number = 0;
+  // percentSent: number = 0;
+  // percentReturn: number = 0;
   @ViewChild('doughnutCanvas') doughnutCanvas;
   @ViewChild('lineCanvas') lineCanvas;
   doughnutChart: any;
@@ -26,7 +36,7 @@ export class HomePage {
   shop = {} as ShopModel;
   constructor(
     public navCtrl: NavController,
-    public orderService: OrderService,
+    public homeService: HomeService,
     public loadingCtrl: LoadingController,
     public menuController: MenuController,
     public app: App
@@ -50,51 +60,51 @@ export class HomePage {
 
     let loading = this.loadingCtrl.create();
     loading.present();
-    this.orderService.getOrderByShop(shop._id).then(data => {
+    this.homeService.getHomeSeller(shop._id).then(data => {
 
-      // this.homeData = data;
-      this.waiting = data.waiting.length;
-      this.accept = data.accept.length;
-      this.sent = data.sent.length;
-      this.return = data.return.length;
-      this.total = (this.waiting + this.accept + this.sent + this.return);
-      this.percentWaiting = (this.waiting / this.total) * 100;
-      this.percentAccept = (this.accept / this.total) * 100;
-      this.percentSent = (this.sent / this.total) * 100;
-      this.percentReturn = (this.return / this.total) * 100;
-      if (this.doughnutCanvas) {
-        this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      this.homeData = data;
+      console.log(this.homeData);
+      // this.waiting = data.waiting.length;
+      // this.accept = data.accept.length;
+      // this.sent = data.sent.length;
+      // this.return = data.return.length;
+      // this.total = (this.waiting + this.accept + this.sent + this.return);
+      // this.percentWaiting = (this.waiting / this.total) * 100;
+      // this.percentAccept = (this.accept / this.total) * 100;
+      // this.percentSent = (this.sent / this.total) * 100;
+      // this.percentReturn = (this.return / this.total) * 100;
+      // if (this.doughnutCanvas) {
+      //   this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
 
-          type: 'doughnut',
-          data: {
-            labels: ["waiting", "accept", "sent", "return"],
-            datasets: [{
-              label: '# of Votes',
-              data: [this.percentWaiting, this.percentAccept, this.percentSent, this.percentReturn],
-              // data: [this.percentWaiting, this.percentAccept, this.percentSent, this.percentReturn],
-              backgroundColor: [
-                "#D8625D",
-                "#5C926E",
-                "#FFCE56",
-                "#36A2EB"
-              ]
-            }]
-          }, legend: {
-            align: "right",
-            layout: "vertical",
-            // verticalAlign: 'top',
-            x: 40,
-            y: 0
-          }
+      //     type: 'doughnut',
+      //     data: {
+      //       labels: ["waiting", "accept", "sent", "return"],
+      //       datasets: [{
+      //         label: '# of Votes',
+      //         data: [this.percentWaiting, this.percentAccept, this.percentSent, this.percentReturn],
+      //         // data: [this.percentWaiting, this.percentAccept, this.percentSent, this.percentReturn],
+      //         backgroundColor: [
+      //           "#D8625D",
+      //           "#5C926E",
+      //           "#FFCE56",
+      //           "#36A2EB"
+      //         ]
+      //       }]
+      //     }, legend: {
+      //       align: "right",
+      //       layout: "vertical",
+      //       // verticalAlign: 'top',
+      //       x: 40,
+      //       y: 0
+      //     }
 
-        });
-      }
+      //   });
+      // }
       if (this.lineCanvas) {
-
         this.lineChart = new Chart(this.lineCanvas.nativeElement, {
           type: 'line',
           data: {
-            labels: ["waiting", "accept", "sent", "return"],
+            labels: [this.homeData.report[3].title, this.homeData.report[2].title, this.homeData.report[1].title, this.homeData.report[0].title],
             datasets: [
               {
                 label: "",
@@ -115,7 +125,8 @@ export class HomePage {
                 pointHoverBorderWidth: 2,
                 pointRadius: 1,
                 pointHitRadius: 10,
-                data: [this.waiting, this.accept, this.sent, this.return],
+                // data:[12000,2300,300,100000],
+                data: [this.homeData.report[3].amount, this.homeData.report[2].amount, this.homeData.report[1].amount, this.homeData.report[0].amount],
                 spanGaps: false,
               }
             ]
